291ce3e78c424c12cb20594d95018bf1af299001
Raghavjit Rana <raghavjit002@gmail.com>
2024-04-29 21:23:51 +0530

    fixed render error

--- src/App.jsx
+++ src/App.jsx
@@ -47,5 +47,5 @@ export default function App() {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
