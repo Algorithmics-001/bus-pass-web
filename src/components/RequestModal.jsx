ba40048ea5fc365feedecb3f95990ffa38c82e3c
Raghavjit Rana <raghavjit002@gmail.com>
2024-04-30 05:45:46 +0530

    refesh on click again

--- src/components/RequestModal.jsx
+++ src/components/RequestModal.jsx
@@ -5,7 +5,7 @@ const RequestModal = ({ student_id, user_id, info, requestType, admin, setTrigge
   const hideModal = (modalId) => {
     const modal = document.getElementById(modalId);
     if (modal) {
-      modal.classList.remove('open'); 
+      modal.removeAttribute('open')
     }
   };
 
@@ -82,17 +82,17 @@ const RequestModal = ({ student_id, user_id, info, requestType, admin, setTrigge
           <h3 className="font-bold text-lg">{info}</h3>
             {(requestType[0] === 'a') && (
               <>
-                {(requestType[1] !== '?') && <button className="btn btn-outline btn-primary m-2" onClick={() => handleAccount('?')}>Restore</button>}
-                {(requestType[1] !== 'a') && <button className="btn btn-outline btn-primary m-2" onClick={() => handleAccount('a')}>Accept</button>}
-                {(requestType[1] !== 'r') && <button className="btn btn-outline btn-primary m-2" onClick={() => handleAccount('r')}>Reject</button>}
+                {(requestType[1] !== '?') && <button className="btn btn-outline btn-primary ml-4" onClick={() => handleAccount('?')}>Restore</button>}
+                {(requestType[1] !== 'a') && <button className="btn btn-outline btn-primary ml-4" onClick={() => handleAccount('a')}>Accept</button>}
+                {(requestType[1] !== 'r') && <button className="btn btn-outline btn-primary ml-4" onClick={() => handleAccount('r')}>Reject</button>}
               </>
             )}
 
             {(requestType[0] === 'b') && (
               <>
-                {(requestType[4] !== '?') && <button className="btn btn-outline btn-primary m-2" onClick={() => handlePass('?')}>Restore</button>}
-                {(requestType[4] !== 'a') && <button className="btn btn-outline btn-primary m-2" onClick={() => handlePass('a')}>Accept</button>}
-                {(requestType[4] !== 'r') && <button className="btn btn-outline btn-primary m-2" onClick={() => handlePass('r')}>Reject</button>}
+                {(requestType[4] !== '?') && <button className="btn btn-outline btn-primary ml-4" onClick={() => handlePass('?')}>Restore</button>}
+                {(requestType[4] !== 'a') && <button className="btn btn-outline btn-primary ml-4" onClick={() => handlePass('a')}>Accept</button>}
+                {(requestType[4] !== 'r') && <button className="btn btn-outline btn-primary ml-4" onClick={() => handlePass('r')}>Reject</button>}
               </>
             )}
           <div className="modal-action">
