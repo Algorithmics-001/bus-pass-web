3866853a54fbc72c07184894556289a0f892d097
Raghavjit Rana <raghavjit002@gmail.com>
2024-04-30 09:15:13 +0530

    Revert "removed bulk and redirect on login"
    This reverts commit ab349db175a3fd3a875c2589c7305d49b4207f62.
    
    fatt gya

--- src/pages/bus-service/BusServiceLogin.jsx
+++ src/pages/bus-service/BusServiceLogin.jsx
@@ -1,5 +1,5 @@
 import React ,{ useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import axios from 'axios';
 import Cookies from 'js-cookie'
